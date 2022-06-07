22c65c01786779ee9cfdd6b9aee44d9c730fa947
Hugo Osvaldo Barrera <hugo@barrera.io>
2022-06-07 18:49:27 +0200

    Clean up `unwrap`s everywhere except `main()`

--- src/main.rs
+++ src/main.rs
@@ -12,58 +12,74 @@ mod proxy_socket;
 
 use proxy_socket::ProxySocket;
 
-const BUFFER_SIZE: usize = 1024 ^ 2;	 // 1024 ^ 2 is the maximum
+const BUFFER_SIZE: usize = 1024 ^ 2; // 1024 ^ 2 is the maximum
 
 fn valid_length(length: usize) -> bool {
     length > 0 && length <= BUFFER_SIZE
 }
 
-fn read_header() -> usize {
+// Read a header (message size) from stdin (e.g.: from the browser).
+fn read_header() -> Result<usize> {
     let stdin = stdin();
     let mut buf = vec![0; 4];
     let mut handle = stdin.lock();
 
-    handle.read_exact(&mut buf).unwrap();
-    NativeEndian::read_u32(&buf).try_into().unwrap()
+    handle.read_exact(&mut buf)?;
+
+    NativeEndian::read_u32(&buf)
+        .try_into()
+        .map_err(|err| Error::new(ErrorKind::InvalidData, err))
 }
 
-fn read_body<T: Read + Write>(length: usize, socket: &mut ProxySocket<T>) {
+// Handle a whole request/response cycle
+//
+// Read a message body from stdin (e.g.: from the browser), and echo it back to the browser's
+// socket. Then await a response from the socket and relay that back to the browser.
+fn read_body<T: Read + Write>(length: usize, socket: &mut ProxySocket<T>) -> Result<()> {
     let mut buffer = vec![0; length];
     let stdin = stdin();
     let mut handle = stdin.lock();
 
-    if handle.read_exact(&mut buffer).is_ok() && valid_length(length) {
-        socket.write_all(&buffer).unwrap();
-        socket.flush().unwrap();
-        read_response(socket);
+    handle.read_exact(&mut buffer)?;
+
+    if valid_length(length) {
+        socket.write_all(&buffer)?;
+        socket.flush()?;
+        read_response(socket)?;
     }
+
+    Ok(())
 }
 
-fn read_response<T: Read>(socket: &mut ProxySocket<T>) {
+// Read a response (from KP's socket) and echo it back to the browser.
+fn read_response<T: Read>(socket: &mut ProxySocket<T>) -> Result<()>{
     let mut buf = vec![0; BUFFER_SIZE];
     if let Ok(len) = socket.read(&mut buf) {
-        write_response(&buf[0..len]);
+        write_response(&buf[0..len])?;
     }
+
+    Ok(())
 }
 
-fn write_response(buf: &[u8]) {
+// Write a response to stdout (e.g.: to the browser).
+fn write_response(buf: &[u8]) -> Result<()> {
     let stdout = stdout();
     let mut out = stdout.lock();
 
-    out.write_u32::<NativeEndian>(buf.len() as u32).unwrap();
-    out.write_all(buf).unwrap();
-    out.flush().unwrap();
+    out.write_u32::<NativeEndian>(buf.len() as u32)?;
+    out.write_all(buf)?;
+    out.flush()?;
+
+    Ok(())
 }
 
 fn main() {
     let mut socket = proxy_socket::connect(BUFFER_SIZE).unwrap();
 
     // Start thread for user input reading
-    let ui = thread::spawn(move || {
-        loop {
-            let length = read_header();
-            read_body(length, &mut socket);
-        }
+    let ui = thread::spawn(move || loop {
+        let length = read_header().unwrap();
+        read_body(length, &mut socket).unwrap();
     });
 
     let _ui_res = ui.join().unwrap();
