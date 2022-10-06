0c680046f91b4ddf06836a5136052c28669dcf2b
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2022-10-06 21:40:21 +0200

    Exit if/when Firefox exits

--- src/main.rs
+++ src/main.rs
@@ -43,28 +43,33 @@ fn socket_to_stdout<T: Read + Write>(socket: &mut ProxySocket<T>) -> Result<()> 
     let mut out = stdout().lock();
     let mut buf = [0; BUFFER_SIZE];
 
-    while let Ok(len) = socket.read(&mut buf) {
-        // If a message is larger than the maximum, ignore it entirely. These are disallowed
-        // by the browser anyway, so sending one would be a protocol violation.
-        if len <= BUFFER_SIZE {
-            out.write_u32::<NativeEndian>(len as u32)?;
-            out.write_all(&buf[..len])?;
-            out.flush()?;
+    loop {
+        if let Ok(len) = socket.read(&mut buf) {
+            // If a message is larger than the maximum, ignore it entirely. These are disallowed
+            // by the browser anyway, so sending one would be a protocol violation.
+            if len <= BUFFER_SIZE {
+                out.write_u32::<NativeEndian>(len as u32)?;
+                out.write_all(&buf[..len])?;
+                out.flush()?;
+            };
+        } else {
+            // TOOD: is the socket is closed, we should try to reconnect.
+
+            return Err(Error::from(ErrorKind::BrokenPipe));
         }
     }
-
-    Err(Error::from(ErrorKind::BrokenPipe))
 }
 
 fn main() -> Result<()> {
     let mut socket = proxy_socket::connect(BUFFER_SIZE)?;
     let mut socket_clone = socket.try_clone()?;
 
-    thread::scope(|s| {
-        // When either of these panic, we exit immediately.
-        s.spawn(move || stdin_to_socket(&mut socket).unwrap());
-        s.spawn(move || socket_to_stdout(&mut socket_clone).unwrap());
-    });
+    thread::spawn(move || socket_to_stdout(&mut socket_clone).unwrap());
+
+    // If stdin is closed, that means that Firefox has exited, so we exit too.
+    // If the socket is closed, this will (eventually) fail too, however, this can later be
+    // refactored to reconnect the underlying ProxySocket.
+    stdin_to_socket(&mut socket).unwrap();
 
     Ok(())
 }
